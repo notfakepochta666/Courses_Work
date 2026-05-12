604caa7ebe73bfd8e58c7a0fe9026cca2b15113a
notfakepochta666 <notfakepochta666@gmail.com>
2026-05-12 22:19:07 +0300

    Feature: Add button hover highlighting to MenuScene

--- Scenes/MenuScene.h
+++ Scenes/MenuScene.h
@@ -11,9 +11,19 @@ private:
     sf::FloatRect settingsBtn;
     sf::FloatRect exitBtn;
 
+    // Для подсветки кнопок
+    sf::RectangleShape playBtnHighlight;
+    sf::RectangleShape settingsBtnHighlight;
+    sf::RectangleShape exitBtnHighlight;
+
+    bool isPlayHovered = false;
+    bool isSettingsHovered = false;
+    bool isExitHovered = false;
+
 public:
     MenuScene();
 
     void handleEvent(sf::Event& event, sf::RenderWindow& window, int& state);
+    void update(sf::RenderWindow& window);
     void draw(sf::RenderWindow& window);
 };
